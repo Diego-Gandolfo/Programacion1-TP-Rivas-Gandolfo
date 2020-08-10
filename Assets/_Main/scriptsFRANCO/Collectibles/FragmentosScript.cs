c443c7b404f392d5de0e159534eb1f799e16bc24
Franco Rivas <francorivaa19@gmail.com>
2020-08-10 03:20:48 -0300

    Script de contador de fragmentos

---
+++ Assets/_Main/scriptsFRANCO/Collectibles/FragmentosScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragmentosScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TopDownCharacterMovement_vDiego TopDownCharacter = collision.GetComponent<TopDownCharacterMovement_vDiego>();
+        if (TopDownCharacter != null)
+        {
+            CounterScript.Amount += 1;
+            Destroy(gameObject);
+        }
+            
+    }
+}
