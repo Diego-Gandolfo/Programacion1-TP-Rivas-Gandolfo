244962fc760d98aa0c118d7367acf99c7009396c
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-11 17:05:13 -0300

    Principio de Scripts Character_Attack01 y Character_AttackArea01

---
+++ Assets/_Main/scriptsDIEGO/Characters & NPC/Characer_AttackArea01.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characer_AttackArea01 : MonoBehaviour
+{
+    public List<Collider2D> collidersInAttackArea = new List<Collider2D>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collidersInAttackArea.Contains(collision)) // Se fija si el Collider no está en la Lista.
+        {
+            collidersInAttackArea.Add(collision); // Si no lo está, lo agrega.
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collidersInAttackArea.Contains(collision)) // Se fija si el Collider está en la Lista
+        {
+            collidersInAttackArea.Remove(collision); // Si está, lo saca.
+        }
+    }
+}
---
+++ Assets/_Main/scriptsDIEGO/Characters & NPC/Character_Attack01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Attack01 : MonoBehaviour
+{
+    public string axisFire = "Fire1";
+    public GameObject attackArea;
+    public LayerMask m_LayerMask;
+    public float damage;
+    public float delay;
+
+
+    private void Update()
+    {
+        if (Input.GetAxis(axisFire) != 0)
+        {
+            ListObjects(); 
+        }
+    }
+
+    private void ListObjects()
+    {
+        Characer_AttackArea01 char_AttackArea01 = attackArea.GetComponent<Characer_AttackArea01>();
+
+        foreach (Collider2D collider in char_AttackArea01.collidersInAttackArea)
+        {
+            Debug.Log(collider + " > " + collider.gameObject.name);
+        }
+    }
+}
