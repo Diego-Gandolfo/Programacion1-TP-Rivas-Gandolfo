a8f8fae2c0884e7f7eb2d40d6727059b55dd472f
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-13 03:52:49 -0300

    Ataque Melee Mejorado

--- Assets/- Diego/Scripts/Characters & NPC/Character_Attack01.cs
+++ Assets/- Diego/Scripts/Characters & NPC/Character_Attack01.cs
@@ -4,28 +4,37 @@ using UnityEngine;
 
 public class Character_Attack01 : MonoBehaviour
 {
-    public string axisFire = "Fire1";
-    public GameObject attackArea;
-    public LayerMask m_LayerMask;
-    public float damage;
-    public float delay;
+    [Header("Attack Area")]
+    public Transform attackPosition;
+    public LayerMask targetLayerMask;
+    public float attackRange = 1.0f;
 
+    [Header("Attack Settings")]
+    public float damage = 10.0f;
+    public float attackCooldown = 1.0f;
 
     private void Update()
     {
-        if (Input.GetAxis(axisFire) != 0)
+        if (Input.GetMouseButtonDown(0))
         {
-            ListObjects(); 
+            Attack(); 
         }
     }
 
-    private void ListObjects()
+    private void Attack()
     {
-        Characer_AttackArea01 char_AttackArea01 = attackArea.GetComponent<Characer_AttackArea01>();
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, targetLayerMask);
 
-        foreach (Collider2D collider in char_AttackArea01.collidersInAttackArea)
+        foreach (Collider2D enemy in hitEnemies)
         {
-            Debug.Log(collider + " > " + collider.gameObject.name);
+            Debug.Log("Hit " + enemy.name);
         }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPosition == null) return;
+
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
     }
 }
