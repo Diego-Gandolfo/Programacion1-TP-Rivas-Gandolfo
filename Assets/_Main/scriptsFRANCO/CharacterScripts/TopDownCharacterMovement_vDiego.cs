e6f2869a2d25205026b4b05b7fe46820f7dec8f5
Franco Rivas <francorivaa19@gmail.com>
2020-08-13 03:24:14 -0300

    animación de correr (solo para abajo por hoy)

--- Assets/_Main/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
+++ Assets/_Main/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
@@ -9,22 +9,30 @@ public class TopDownCharacterMovement_vDiego : MonoBehaviour
     public int health = 10;
     public Animator animator;
 
+
     void Update()
     {
         var direction = Vector3.zero;
 
         if (Input.GetKey(KeyCode.W)) direction += Vector3.up; // Si los IF solo tienen una línea de código no necesitan las {} y eso simplifica la lectura
-        if (Input.GetKey(KeyCode.S)) direction -= Vector3.up;
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction -= Vector3.up;
+            animator.SetBool("Run", true);
+        }
         if (Input.GetKey(KeyCode.A)) direction -= Vector3.right;
         if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
 
         if (Input.GetButtonDown("Fire1"))
             animator.SetTrigger("Attack");
 
-        animator.SetFloat("Back", direction.x);
+        //PARA QUE VUELVA A ESTADO IDLE!
+        if (Input.GetKeyUp(KeyCode.S)) animator.SetBool("Run", false);
+
+        /*animator.SetFloat("Back", direction.x);
         animator.SetFloat("Front", direction.y);
         animator.SetFloat("Speed", direction.magnitude);
-
+        */
         transform.position += direction * speed * Time.deltaTime; // PARA QUE NO SE COMPENSEN LAS VELOCIDADES CUANDO TOCAS POR EJEMPLO A Y W
 
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // En lugar de tener una Variable mainCamera se puede usar directamente Camera.main
