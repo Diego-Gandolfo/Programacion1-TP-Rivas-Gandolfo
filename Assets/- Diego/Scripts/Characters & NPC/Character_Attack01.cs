bb611531f54a0c47e52b47b3f190ca3733c01961
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-13 04:11:44 -0300

    Terminando ajustes finales del Character_Attack01

--- Assets/- Diego/Scripts/Characters & NPC/Character_Attack01.cs
+++ Assets/- Diego/Scripts/Characters & NPC/Character_Attack01.cs
@@ -6,35 +6,39 @@ public class Character_Attack01 : MonoBehaviour
 {
     [Header("Attack Area")]
     public Transform attackPosition;
-    public LayerMask targetLayerMask;
+    public LayerMask targetsLayerMask;
     public float attackRange = 1.0f;
 
     [Header("Attack Settings")]
     public float damage = 10.0f;
     public float attackCooldown = 1.0f;
+    private float cooldownTimer = 0.0f;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if ((Input.GetMouseButtonDown(0)) && (Time.time >= cooldownTimer))
         {
-            Attack(); 
+            Attack();
+            cooldownTimer = Time.time + attackCooldown;
         }
     }
 
     private void Attack()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, targetLayerMask);
+        Collider2D[] targetsHit = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, targetsLayerMask);
 
-        foreach (Collider2D enemy in hitEnemies)
+        foreach (Collider2D target in targetsHit)
         {
-            Debug.Log("Hit " + enemy.name);
+            Debug.Log("Hit " + target.name);
+            if (target.GetComponent<Health>() != null)
+            {
+                target.GetComponent<Health>().TakeDamage(damage);
+            }
         }
     }
 
     private void OnDrawGizmosSelected()
     {
-        if (attackPosition == null) return;
-
-        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+        if (attackPosition != null) Gizmos.DrawWireSphere(attackPosition.position, attackRange);
     }
 }
