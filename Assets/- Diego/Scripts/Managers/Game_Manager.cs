829e5b5434929df61a53813a52caf038dcb77be0
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 15:49:03 -0300

    Game_Manager y Singleton
    Antes eran un solo Script, pero los separé porque hay otros Objetos que deberán ser Singleton y así podemos aprovechar el mismo Script

--- Assets/- Diego/Scripts/Managers/Game_Manager.cs
+++ Assets/- Diego/Scripts/Managers/Game_Manager.cs
@@ -1,26 +1,18 @@
-﻿using System.Collections;
+﻿/*
+ * Este Script lo usaremos para almacenar y manejar distintas cosas del juego
+ * 
+ * Lo usaremos para almacenar los datos que vayamos a guardar
+ * 
+ * Lo usaremos para almacenar todo tipo de configuración que pueda hacer el Jugador
+ * 
+ * Este Script no debería duplicarse nunca, así que al Objeto que se le asigne hay que hacerlo un Singleton
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Game_Manager : MonoBehaviour
 {
-    public static Game_Manager instance;
 
-    public void Awake()
-    {
-        MakeSingleton();
-    }
-
-    private void MakeSingleton()
-    {
-        if (instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-    }
 }
