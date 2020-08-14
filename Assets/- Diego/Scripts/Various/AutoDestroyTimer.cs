4cf400a372ed66dac813e2499a5ba6880e029125
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 15:04:30 -0300

    Comentarios AutoDestroyTimer

--- Assets/- Diego/Scripts/Various/AutoDestroyTimer.cs
+++ Assets/- Diego/Scripts/Various/AutoDestroyTimer.cs
@@ -1,26 +1,34 @@
-﻿using System.Collections;
+﻿/*
+ * Este contador cuenta desde timeToDestroy hasta 0 y una vez que termina destruye el Objeto.
+ * 
+ * Si queremos detener el contador, hay que desactivar el Objeto.
+ * 
+ * Si queremos resetearlo para que empiece a contar de nuevo, se usa el ResetTimer.
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AutoDestroyTimer : MonoBehaviour
 {
-    public float timeToDestroy = 600;
+    public float timeToDestroy = 600; // Variable donde asignamos el tiempo a contar
 
-    private float timer;
+    private float timer; // Variable que usamos para llevar la cuenta
 
     private void Start()
     {
-        timer = timeToDestroy;
+        timer = timeToDestroy; // Inicializamos el timer con el timeToDestroy
     }
 
     private void Update()
     {
-        if (timer > 0) timer -= Time.deltaTime;
-        else if (timer <= 0) Destroy(gameObject);
+        if (timer > 0) timer -= Time.deltaTime; // Si el timer es mayor a 0 le descontamos Time.deltaTime
+        else if (timer <= 0) Destroy(gameObject); // Si es menor o igual a 0 es que termino de contar y destruimos el Objeto
     }
 
     public void ResetTimer()
     {
-        timer = timeToDestroy;
+        timer = timeToDestroy; // Función de Reset por las dudas de que necesitemos reiniciar el contador
     }
 }
