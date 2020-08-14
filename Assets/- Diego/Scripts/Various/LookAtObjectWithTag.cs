bd474de0220a5a472fea32c2db56bfa570c2f9d9
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 15:33:49 -0300

    Update LookAtObjectWithTag.cs

--- Assets/- Diego/Scripts/Various/LookAtObjectWithTag.cs
+++ Assets/- Diego/Scripts/Various/LookAtObjectWithTag.cs
@@ -1,21 +1,29 @@
-﻿using System.Collections;
+﻿/*
+ * Este Script lo que hace es que el Objeto al que se lo asigna mire a donde está el Objeto con un determinado Tag
+ * 
+ * Funciona para Objetos 2D
+ * 
+ * Si hay varios Objetos con el mismo Tag podría no funcionar correctamente
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtObjectWithTag : MonoBehaviour
 {
-    public string tagObject;
-    private GameObject objectWithTag;
+    public string tagObject; // Asignamos el Tag del Objeto a mirar
+    private GameObject objectWithTag; // Alacenaremos el Objeto con el Tag a mirar
 
     private void Start()
     {
-        objectWithTag = GameObject.FindGameObjectWithTag(tagObject);
+        objectWithTag = GameObject.FindGameObjectWithTag(tagObject); // Buscamos el Objeto que tiene el Tag elegido
     }
 
     private void Update()
     {
-        Vector2 direction = new Vector2(objectWithTag.transform.position.x - transform.position.x, objectWithTag.transform.position.y - transform.position.y);
+        Vector2 direction = new Vector2(objectWithTag.transform.position.x - transform.position.x, objectWithTag.transform.position.y - transform.position.y); // Calculamos la dirección a la que hay que mirar
 
-        transform.up = direction;
+        transform.up = direction; // Actualizamos el Transform para que mire al Objeto con el Tag elegido
     }
 }
