1fb0aba84b196ecd9bd7926d9e5ac69569ee5192
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 15:09:02 -0300

    Update ConstantRotation.cs

--- Assets/- Diego/Scripts/Various/ConstantRotation.cs
+++ Assets/- Diego/Scripts/Various/ConstantRotation.cs
@@ -1,15 +1,21 @@
-﻿using System.Collections;
+﻿/*
+ * Este Script es bastante básico, pero lo comento igualmente.
+ * 
+ * Lo que hace es
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstantRotation : MonoBehaviour
 {
-    public Vector3 speedRotation;
+    public Vector3 speedRotation; // Variable para asignar la Velocidad de Rotacion en los 3 Ejes
 
     void Update()
     {
-        Vector3 rotation = speedRotation * Time.deltaTime;
+        Vector3 rotation = speedRotation * Time.deltaTime; // Variable para normalizar con Time.deltaTime la Velocidad de Rotacion
 
-        transform.Rotate(rotation);
+        transform.Rotate(rotation); // Rotamos el Objeto
     }
 }
