5cf49c81bab36c2af99dd6448f540ac56db13dcb
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 16:41:13 -0300

    Update Character_Movement.cs

--- Assets/- Diego/Scripts/Characters & NPC/Character_Movement.cs
+++ Assets/- Diego/Scripts/Characters & NPC/Character_Movement.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections;
+﻿/*
+ * Script para controlar el Movimiento del Personaje
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Character_Movement : MonoBehaviour
 {
     // Variables de Movimiento
-    public float movementSpeed = 7;
-    public float runMultiplier = 3;
+    public float movementSpeed = 7; // Configuramos la Velocidad de Movimiento
+    public float runMultiplier = 3; // Configuramos un Multiplicador para cuando corre
 
     private void Update()
     {
         // Movimiento
-        float speedMovVer = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.LeftShift)) speedMovVer *= runMultiplier;
+        float speedMovVer = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime; // Almacenamos el Eje Vertical multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
+        if (Input.GetKey(KeyCode.LeftShift)) speedMovVer *= runMultiplier; // Si presiona el Shift Izquierdo aplicamos el Multiplicador de Velocidad para que corra
 
-        float speedMovHor = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.LeftShift)) speedMovHor *= runMultiplier;
+        float speedMovHor = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime; // Almacenamos el Eje Horizontal multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
+        if (Input.GetKey(KeyCode.LeftShift)) speedMovHor *= runMultiplier; // Si presiona el Shift Izquierdo aplicamos el Multiplicador de Velocidad para que corra
 
-        transform.Translate(0, speedMovVer, 0);
-        transform.Translate(speedMovHor, 0, 0);
+        transform.Translate(0, speedMovVer, 0); // Nos movemos en el Eje Vertical
+        transform.Translate(speedMovHor, 0, 0); // Nos movemos en el Eje Horizontal
     }
 }
