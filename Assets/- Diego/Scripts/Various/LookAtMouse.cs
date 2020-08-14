f22f69fc2149144a035714ba0c0c6dbf2a8b6816
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 15:30:36 -0300

    Update LookAtMouse.cs

--- Assets/- Diego/Scripts/Various/LookAtMouse.cs
+++ Assets/- Diego/Scripts/Various/LookAtMouse.cs
@@ -1,14 +1,20 @@
-﻿using UnityEngine;
+﻿/*
+ * Este Script lo que hace es que el Objeto al que se lo asigna mire a donde está el puntero del Mouse
+ * 
+ * Funciona para Objetos 2D
+*/ 
+
+using UnityEngine;
 using System.Collections;
 
-public class LookAtMouse : MonoBehaviour
+public class LookAtMouse2D : MonoBehaviour
 {
     private void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Almacenamos las coordenadas de donde se encuentra el puntero del Mouse
 
-        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y); // Calculamos la dirección a la que hay que mirar
 
-        transform.up = direction;
+        transform.up = direction; // Actualizamos el Transform para que mire al puntero del Mouse
     }
 }
