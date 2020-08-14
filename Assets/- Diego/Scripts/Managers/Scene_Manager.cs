688c9c6e016c1822043426df04ac5ef25f4aeec3
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-14 16:14:45 -0300

    Update Scene_Manager.cs

--- Assets/- Diego/Scripts/Managers/Scene_Manager.cs
+++ Assets/- Diego/Scripts/Managers/Scene_Manager.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿/*
+ * Este Script lo vamos a usar para Configurar la Escena
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class Scene_Manager : MonoBehaviour
 {
-    public void Load_Scene(string sceneName)
+    public void Load_Scene(string sceneName) // Función para cargar una nueva Escena, útil en las Escenas de Menu
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName); // Cargamos la Escena deseada
     }
 }
