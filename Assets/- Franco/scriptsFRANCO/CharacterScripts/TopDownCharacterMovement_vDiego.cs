b3e74899c9219b0941e8dbf7aaa542ce1ecd6b4b
Franco Rivas <francorivaa19@gmail.com>
2020-08-14 13:30:05 -0300

    Run Up animación y enemigo

--- Assets/- Franco/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
+++ Assets/- Franco/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
@@ -14,7 +14,11 @@ public class TopDownCharacterMovement_vDiego : MonoBehaviour
     {
         var direction = Vector3.zero;
 
-        if (Input.GetKey(KeyCode.W)) direction += Vector3.up; // Si los IF solo tienen una línea de código no necesitan las {} y eso simplifica la lectura
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector3.up;
+            animator.SetBool("RunUp", true);
+        }
         if (Input.GetKey(KeyCode.S))
         {
             direction -= Vector3.up;
@@ -38,19 +42,21 @@ public class TopDownCharacterMovement_vDiego : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.S)) animator.SetBool("Run", false);
         if (Input.GetKeyUp(KeyCode.A)) animator.SetBool("RunLeft", false);
         if (Input.GetKeyUp(KeyCode.D)) animator.SetBool("RunRight", false);
-
+        if (Input.GetKeyUp(KeyCode.W)) animator.SetBool("RunUp", false);
+     
         /*animator.SetFloat("Back", direction.x);
         animator.SetFloat("Front", direction.y);
         animator.SetFloat("Speed", direction.magnitude);
         */
         transform.position += direction * speed * Time.deltaTime; // PARA QUE NO SE COMPENSEN LAS VELOCIDADES CUANDO TOCAS POR EJEMPLO A Y W
 
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // En lugar de tener una Variable mainCamera se puede usar directamente Camera.main
+        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // En lugar de tener una Variable mainCamera se puede usar directamente Camera.main
         Vector3 difference = mousePosition - this.transform.position;
 
         float angle = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Euler(0, 0, -angle);
+        */
     }
 
     public void TakeEnemyDamage(int damage)
---
+++ Assets/- Franco/scriptsFRANCO/FollowEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowEnemy : MonoBehaviour
+{
+    public float speed;
+    public float stopDistance;
+    public float retreatDistance;
+    public Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stopDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stopDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        {
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+    }
+}
