59e9051864a2b4688a1a27b4a953d1411ab2250f
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-18 14:39:02 -0300

    Update Patrol_Area.cs

--- Assets/- Diego/Scripts/Characters & NPC/Patrol_Area.cs
+++ Assets/- Diego/Scripts/Characters & NPC/Patrol_Area.cs
@@ -4,32 +4,36 @@ using UnityEngine;
 
 public class Patrol_Area : MonoBehaviour
 {
+    [Header("Enemy Settings")]
+    public float sizeX; // El tamaño del Enemigo en el Eje X
+    public float sizeY; // El tamaño del Enemigo en el Eje Y
+
+    [Header("Patrol Settings")]
     public float movemetSpeed = 3.0f; // La velocidad que se desplaza al estar patrullando
     public float waitTime = 1.0f; // El tiempo que espera hasta en un punto antes de empezar a moverse al siguiente
     private float timer = 0.0f; // Variable que usaremos para llevar el control del tiempo
 
+    [Header("Patrol Point Settings")]
     public Transform patrolPoint; // Variable donde almacenaremos el centro del punto a patrullar
+    private Vector3 positionSquere; // Almacenaremos la posicion del patrolPoint al activar el Objeto o el Componente
     public float minX; // Cuantas unidades se puede mover hacia la izquierda de patrolPoint
     public float maxX; // Cuantas unidades se puede mover hacia la derecha de patrolPoint
     public float minY; // Cuantas unidades se puede mover hacia la abajo de patrolPoint
     public float maxY; // Cuantas unidades se puede mover hacia la arriba de patrolPoint
-
     private float relativelMinX; // Para almacenar el valor minX relativo, sin perder el valor de minX original
     private float relativeMaxX; // Para almacenar el valor maxX relativo, sin perder el valor de maxX original
     private float relativeMinY; // Para almacenar el valor minY relativo, sin perder el valor de minY original
     private float relativeMaxY; // Para almacenar el valor maxY relativo, sin perder el valor de maxY original
-
     private float widthSquere; // Para almacenar el ancho del area
     private float heightSquere; // Para almacenar el alto del area
-    private Vector3 positionSquere; // Almacenaremos la posicion del patrolPoint al activar el Objeto o el Componente
 
     public float minDistance; // Distancia minima que tiene que haber entre al
 
     private void OnEnable() // Cada vez que se activa el Objeto o el Componente
     {
         positionSquere = patrolPoint.position; // Inicializamos la posicion del patrolPoint
-        widthSquere = Mathf.Abs(minX) + Mathf.Abs(maxX) + gameObject.transform.localScale.x; // Inicializamos el ancho del Area
-        heightSquere = Mathf.Abs(minY) + Mathf.Abs(maxY) + gameObject.transform.localScale.y; // Inicializamos el alto del Area
+        widthSquere = Mathf.Abs(minX) + Mathf.Abs(maxX) + sizeX; // Inicializamos el ancho del Area sumando los valores absolutos de minX, maxX y sizeX
+        heightSquere = Mathf.Abs(minY) + Mathf.Abs(maxY) + sizeY; // Inicializamos el alto del Area sumando los valores absolutos de minY, maxY y sizeY
 
         relativelMinX = minX + patrolPoint.position.x; // Inicializamos el valor relativo de minX
         relativeMaxX = maxX + patrolPoint.position.x; // Inicializamos el valor relativo de maxX
