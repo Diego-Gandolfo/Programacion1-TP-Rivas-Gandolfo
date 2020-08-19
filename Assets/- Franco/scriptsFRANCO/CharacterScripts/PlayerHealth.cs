b759d21d6aecc2b94921b5c173f54b35d91aeb79
Franco Rivas <francorivaa19@gmail.com>
2020-08-19 17:42:53 -0300

    Barra de vida! (Player)

--- Assets/- Franco/scriptsFRANCO/CharacterScripts/PlayerHealth.cs
+++ Assets/- Franco/scriptsFRANCO/CharacterScripts/PlayerHealth.cs
@@ -9,19 +9,28 @@ public class PlayerHealth : MonoBehaviour
 
     public GameObject player;
     public GameObject blood;
+
+    public Health_Bar_Script healthBar;
+
     void Start()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakeDamage(float damage)
     {
         currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+
         Instantiate(blood, transform.position, Quaternion.identity);
+
         if (currentHealth <= 0) Die();
     }
     void Die()
     {
         Destroy(player);
+        healthBar.gameObject.SetActive(false);
     }
 }
--- Assets/- Franco/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
+++ Assets/- Franco/scriptsFRANCO/CharacterScripts/TopDownCharacterMovement_vDiego.cs
@@ -6,15 +6,10 @@ using UnityEngine;
 public class TopDownCharacterMovement_vDiego : MonoBehaviour
 {
     public float speed;
-    public int health = 10;
+
     public Animator animator;
     public ParticleSystem blood;
 
-
-
-    private void Start()
-    {
-    }
     void Update()
     {
         var direction = Vector3.zero;
@@ -52,39 +47,8 @@ public class TopDownCharacterMovement_vDiego : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.D)) animator.SetBool("RunRight", false);
         if (Input.GetKeyUp(KeyCode.W)) animator.SetBool("RunUp", false);
      
-        /*animator.SetFloat("Back", direction.x);
-        animator.SetFloat("Front", direction.y);
-        animator.SetFloat("Speed", direction.magnitude);
-        */
+
         transform.position += direction * speed * Time.deltaTime; // PARA QUE NO SE COMPENSEN LAS VELOCIDADES CUANDO TOCAS POR EJEMPLO A Y W
 
-
-
-
-        /*Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // En lugar de tener una Variable mainCamera se puede usar directamente Camera.main
-        Vector3 difference = mousePosition - this.transform.position;
-
-        float angle = Mathf.Atan2(difference.x, difference.y) * Mathf.Rad2Deg;
-
-        transform.rotation = Quaternion.Euler(0, 0, -angle);
-        */
-    }
-
-    /*public void TakeEnemyDamage(int damage)
-    {
-        Instantiate(blood, transform.position, Quaternion.identity);
-
-
-        health -= damage;
-
-        if (health <= 0)
-            Die();
-        
-    }
-
-    private void Die()
-    {
-        Destroy(this.gameObject);
-    }*/
-    
+    } 
 }
