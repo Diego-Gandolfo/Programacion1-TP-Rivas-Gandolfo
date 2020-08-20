be3b04f058837416b82fffa84b81868a79540e84
Franco Rivas <francorivaa19@gmail.com>
2020-08-19 23:37:28 -0300

    UI un poco más elegante y barra de vida de enemigo con problema.
    la barra del enemigo rota con el personaje, cosa que no queremos.
    Ver más tarde

--- Assets/- Franco/scriptsFRANCO/CharacterScripts/EnemyHealth.cs
+++ Assets/- Franco/scriptsFRANCO/CharacterScripts/EnemyHealth.cs
@@ -6,18 +6,25 @@ public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] private float maxHealth = 10f;
     public float currentHeatlh;
+
+    public Health_Bar_Script enemyHealthBar;
     void Start()
     {
         currentHeatlh = maxHealth;
+        enemyHealthBar.SetMaxHealth(maxHealth);
     }
 
     public void TakePlayerDamage(float damage)
     {
         currentHeatlh -= damage;
+
+        enemyHealthBar.SetHealth(currentHeatlh);
+
         if (currentHeatlh <= 0) Die();
     }
     void Die()
     {
         Destroy(gameObject);
+        enemyHealthBar.gameObject.SetActive(false);
     }
 }
---
+++ Assets/BASURA/Billboard_Script.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Billboard_Script : MonoBehaviour
+{
+    public Transform cam;
+    public Transform enemy;
+    void LateUpdate()
+    {
+        
+    }
+}
