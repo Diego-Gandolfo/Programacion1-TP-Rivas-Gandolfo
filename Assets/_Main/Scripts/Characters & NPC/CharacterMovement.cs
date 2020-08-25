7085e04559db11d17b5b7ca4663ced37a201e7bc
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-25 18:15:17 -0300

    Animaciones Character

--- Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
+++ Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
@@ -12,15 +12,24 @@ namespace OnceUponAMemory.Main
     {
         // Variables de Movimiento
         [SerializeField] private float movementSpeed = 7; // Configuramos la Velocidad de Movimiento
+        private Animator animator;
+
+        private void Start()
+        {
+            animator = gameObject.GetComponentInChildren<Animator>();
+        }
 
         private void Update()
         {
             // Movimiento
-            float speedMovVer = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime; // Almacenamos el Eje Vertical multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
-            float speedMovHor = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime; // Almacenamos el Eje Horizontal multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
+            Vector2 speedMov = new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime); // Almacenamos el Eje Vertical y el Eje Horizontal multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
 
-            transform.Translate(0, speedMovVer, 0); // Nos movemos en el Eje Vertical
-            transform.Translate(speedMovHor, 0, 0); // Nos movemos en el Eje Horizontal
+            transform.Translate(speedMov); // Nos movemos en el Eje Vertical y Horizontal
+
+            // Esto es para probar los Blend Tree, pero la idea es que funcione apuntando a donde esté el Mouse y no en el sentido que se mueve
+            animator.SetFloat("Horizontal", speedMov.x);
+            animator.SetFloat("Vertical", speedMov.y);
+            animator.SetFloat("Speed", speedMov.sqrMagnitude);
         }
     }
 }
