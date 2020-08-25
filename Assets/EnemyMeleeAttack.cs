fa037ab5b899fdd55755613041c549302ff91b38
Franco Rivas <francorivaa19@gmail.com>
2020-08-24 23:48:53 -0300

    comienzo de enemigo melee!

---
+++ Assets/EnemyMeleeAttack.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using OnceUponAMemory.Main;
+using UnityEngine;
+
+namespace OnceUponAMemory.Main
+{
+    public class EnemyMeleeAttack : MonoBehaviour
+    {
+        private int damage = 2;
+        public float attackRate = 2f;
+        private float nextAttack = 0f;
+
+        public Animator animator;
+
+        public Transform player;
+        private void Update()
+        {
+            transform.LookAt(player.up);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            PlayerHealth player = other.GetComponent<PlayerHealth>();
+            if (player != null && Time.time >= nextAttack)
+            {
+                animator.SetTrigger("Attack");
+            
+                player.TakeDamage(damage);
+                nextAttack = Time.time + 1f / attackRate;
+            }
+        }
+    }
+}
+
