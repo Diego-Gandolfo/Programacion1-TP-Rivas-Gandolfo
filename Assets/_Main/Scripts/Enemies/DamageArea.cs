d414a3873c7df1a3533d62e3abffe715c548a1cb
Franco Rivas <francorivaa19@gmail.com>
2020-08-26 18:31:30 -0300

    DAÑO EN ONTRIGGERSTAY SIN TERMINAR

--- Assets/_Main/Scripts/Enemies/DamageArea.cs
+++ Assets/_Main/Scripts/Enemies/DamageArea.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,9 +8,10 @@ namespace OnceUponAMemory.Main
     public class DamageArea : MonoBehaviour
     {
         private int damage = 2;
+        public float damageRate = 2f;
+        private float nextDamage = 0f;
 
-
-        private void OnCollisionEnter2D(Collision2D collision)
+        /*private void OnCollisionEnter2D(Collision2D collision)
         {
             PlayerHealth TopDownCharacter = collision.transform.GetComponent<PlayerHealth>();
             if (TopDownCharacter != null)
@@ -17,7 +19,19 @@ namespace OnceUponAMemory.Main
                 TopDownCharacter.TakeDamage(damage);
                 Debug.Log("im taking damage");
             }
-
+        }
+        */
+        
+        //QUERÍA HACER QUE EL DAÑO FUERA CON ON TRIGGER STAY, PERO POR AHORA NO FUNCIONA!
+        private void OnTriggerStay(Collider other)
+        {
+            PlayerHealth Player = other.transform.GetComponent<PlayerHealth>();
+            if (Player != null && Time.time >= nextDamage)
+            {
+                Player.TakeDamage(damage);
+                nextDamage = Time.time + 1f / damageRate;
+                Debug.Log("im damage");
+            }
         }
     }
 }
