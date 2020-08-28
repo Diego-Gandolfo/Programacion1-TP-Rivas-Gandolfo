445944fa2c8f14797d249b445cdf9ce69e4c0592
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-28 16:44:00 -0300

    HealthBar EnemyMelee

--- Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
+++ Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
@@ -11,19 +11,14 @@ namespace OnceUponAMemory.Main
     public class CharacterMovement : MonoBehaviour
     {
         [SerializeField] private float movementSpeed = 7; // Configuramos la Velocidad de Movimiento
-        private Animator animator;
-
-        private void Start()
-        {
-            animator = gameObject.GetComponentInChildren<Animator>();
-        }
+        [SerializeField] private Animator animatorMovement;
 
         private void Update()
         {
             // Movimiento
             Vector2 speedMov = new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime); // Almacenamos el Eje Vertical y el Eje Horizontal multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
             transform.Translate(speedMov); // Nos movemos en el Eje Vertical y Horizontal
-            animator.SetFloat("Speed", speedMov.sqrMagnitude);
+            animatorMovement.SetFloat("Speed", speedMov.sqrMagnitude);
         }
     }
 }
