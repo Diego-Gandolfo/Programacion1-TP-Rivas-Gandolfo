3156cb0ab302d53cfe7f41685d0ec9e57f97739c
Franco Rivas <francorivaa19@gmail.com>
2020-08-30 18:59:17 -0300

    TEXT MESH PARA HIT COUNTER SIN TERMINAR

---
+++ Assets/_Main/Scripts/Controllers/HitCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/*public class HitCounter : MonoBehaviour
+{
+    private TextMeshPro textMeshPro;
+
+    private void Awake()
+    {
+        textMeshPro = GetComponent<TextMeshPro>();
+    }
+
+    private void SetHitCounter(int hitCount)
+    {
+        textMeshPro.SetText(hitCount.ToString());
+    }
+}*/
+
+//SCRIPT PARA HACER UN HIT COUNTER EN EL PLAYER EN PROCESO
