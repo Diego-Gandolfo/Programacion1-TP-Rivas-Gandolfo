c408f8fed271eff96277f6bbf3b697b6e0309585
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-08-31 19:37:10 -0300

    Comence el DetectTragetArea

---
+++ Assets/- Diego/Scripts/Characters & NPC/DetectTargetArea.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnceUponAMemory.Diego
+{
+    public class DetectTargetArea : MonoBehaviour
+    {
+        [SerializeField] private Transform detectionCenterPoint = null;
+        [SerializeField] private float detectionRange = 1.0f;
+        [SerializeField] private LayerMask targetsLayerMask = 0;
+
+        private void Update()
+        {
+            bool check = DetectTargets();
+
+            if (check)
+            {
+                Debug.Log("Encontre algo! check >> " + check);
+            }
+            else
+            {
+                Debug.Log("check >> " + check);
+            }
+        }
+
+        private bool DetectTargets()
+        {
+            Collider2D[] targetsDetected = Physics2D.OverlapCircleAll(detectionCenterPoint.position, detectionRange, targetsLayerMask); 
+
+            if (targetsDetected.Length > 0) return (true);
+            else return (false);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (detectionCenterPoint != null) Gizmos.DrawWireSphere(detectionCenterPoint.position, detectionRange); // Esto es para dibujar donde está el Overlap
+        }
+
+    }
+}
