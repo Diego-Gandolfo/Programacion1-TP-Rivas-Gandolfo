266411e4b4e302bf1d4b3840ad1748013184c405
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-09-02 18:05:04 -0300

    LookObjectWithTag

---
+++ Assets/- Diego/Scripts/Various/LookAtObject2D.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Este Script lo que hace es que el Objeto al que se lo asigna mire a donde está el Objeto con un determinado Tag
+ * 
+ * Funciona para Objetos 2D
+ * 
+ * Si hay varios Objetos con el mismo Tag podría no funcionar correctamente
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnceUponAMemory.Diego
+{
+    public class LookAtObject2D : MonoBehaviour
+    {
+        [SerializeField] private GameObject target = null; // Alacenaremos el Objeto con el Tag a mirar
+
+        private void Update()
+        {
+            if (target != null)
+            {
+                Vector2 direction = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y); // Calculamos la dirección a la que hay que mirar
+
+                transform.up = direction; // Actualizamos el Transform para que mire al Objeto con el Tag elegido
+            }
+        }
+    }
+}
