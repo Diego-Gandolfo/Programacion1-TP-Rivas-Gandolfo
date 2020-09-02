ccf164e6714d70964dfa20966f44ff67b51d1de4
Franco Rivas <francorivaa19@gmail.com>
2020-09-02 13:50:55 -0300

    ()

--- Assets/_Main/Scripts/Various/FlipSpriteX.cs
+++ Assets/_Main/Scripts/Various/FlipSpriteX.cs
@@ -8,13 +8,19 @@ namespace OnceUponAMemory.Main
     {
         [SerializeField] private SpriteRenderer spriteRenderer = null;
         [SerializeField] private GameObject target = null;
-
+        
         private void Update()
         {
-            if (transform.position.x > target.transform.position.x) // Chequeamos si nuestra posicion en Eje X es MAYOR que la del Player
+            if (transform.position.x > target.transform.position.x)
+            {
+                // Chequeamos si nuestra posicion en Eje X es MAYOR que la del Player
                 spriteRenderer.flipX = true; // Volteamos
-            else // Si no es MAYOR
+            }
+            else 
+            {
                 spriteRenderer.flipX = false; // No volteamos
+            }
+            
         }
     }
 }
