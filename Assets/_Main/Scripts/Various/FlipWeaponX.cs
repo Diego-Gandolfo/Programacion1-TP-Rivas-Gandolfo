3e96085b94ab6d2be92240951dddf89eb8a96316
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-09-03 04:28:29 -0300

    Pruebas para hacer el Flip del Weapon Area del EnemyMelee

---
+++ Assets/_Main/Scripts/Various/FlipWeaponX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnceUponAMemory.Main
+{
+    public class FlipWeaponX : MonoBehaviour
+    {
+        [SerializeField] private SpriteRenderer spriteRenderer = null;
+        //[SerializeField] private GameObject target = null;
+        private float xOriginalPosition = 0f;
+
+        private void Start()
+        {
+            xOriginalPosition = transform.position.x;
+        }
+
+        private void Update()
+        {
+            if (spriteRenderer.flipX)
+            {
+                transform.SetPositionAndRotation(new Vector3(-xOriginalPosition, transform.position.y, transform.position.z), transform.rotation);
+            }
+            else 
+            {
+                transform.SetPositionAndRotation(new Vector3(xOriginalPosition, transform.position.y, transform.position.z), transform.rotation);
+            }
+
+        }
+    }
+}
