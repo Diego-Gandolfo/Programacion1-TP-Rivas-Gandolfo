7954dc0b11f3c79e5475fd6c4a2b695a4c0b5512
Franco Rivas <francorivaa19@gmail.com>
2020-09-07 19:35:39 -0300

    save()

---
+++ Assets/_Main/Scripts/Controllers/SecondTutorialTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using OnceUponAMemory.Main;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecondTutorialTrigger : MonoBehaviour
+{
+    [SerializeField] private Text secondTutorialText;
+
+    private void Start()
+    {
+        secondTutorialText.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerHealth>())
+        {
+            //secondTutorialText.gameObject.SetActive(true);
+            Debug.Log("lol");
+        }
+        
+    }
+}
