96ce7dfd2371e526176c565767f8e2a85ba34f52
Franco Rivas <francorivaa19@gmail.com>
2020-09-08 17:20:26 -0300

    ahora la imágen del segundo tutorial aparece, pero no desaparece

---
+++ Assets/_Main/Scripts/Controllers/SecondTutorialText.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using OnceUponAMemory.Main;
+using UnityEngine;
+
+public class SecondTutorialText : MonoBehaviour
+{
+    [SerializeField] private GameObject image;
+    [SerializeField] private bool canAppear;
+    
+    public void Start()
+    {
+        canAppear = false;
+        image.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerHealth>())
+        {
+            Debug.Log("this works");
+            canAppear = true;
+            image.SetActive(true);
+        }
+    }
+}
