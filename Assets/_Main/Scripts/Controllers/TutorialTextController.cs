955ed6051efabfbbd2fbf5fa7f88801c12b6596d
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-09-08 18:21:12 -0300

    Tutorial Parte 1

--- Assets/_Main/Scripts/Controllers/TutorialTextController.cs
+++ Assets/_Main/Scripts/Controllers/TutorialTextController.cs
@@ -10,12 +10,22 @@ namespace OnceUponAMemory.Main
     {
         [SerializeField] private Text tutorialText;
         [SerializeField] private Animator animator;
+        /*
         private void Update()
         {
             if (Input.anyKey)
             {
                 tutorialText.gameObject.SetActive(false);
                 animator.SetTrigger("KeyPressed");
+            }
+        }*/
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+            {
+                animator.SetTrigger("KeyPressed");
+                Destroy(gameObject, 5f);
             }
         }
     }
