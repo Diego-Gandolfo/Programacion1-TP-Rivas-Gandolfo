19306560c3e6b9e2211848d290a47ed4cd1f9d12
Diego-Gandolfo <Diego.Gandolfo@Gmail.com>
2020-09-09 18:39:56 -0300

    Tutorial en progreso

--- Assets/- Diego/Scripts/Managers/TutorialManager.cs
+++ Assets/- Diego/Scripts/Managers/TutorialManager.cs
@@ -10,63 +10,82 @@ namespace OnceUponAMemory.Diego
 {
     public class TutorialManager : MonoBehaviour
     {
+        [Header("Manager")]
+        [SerializeField] private float timeToStart = 0f; // Almacenaremos el Objeto que contiene el Cartel de Movimiento
+        private bool canStart = false; // Indicaremos cuando completo el Tutorial de Movimiento
+
         [Header("Movement")]
         [SerializeField] private GameObject movementObject = null; // Almacenaremos el Objeto que contiene el Cartel de Movimiento
+        [SerializeField] private float movementTimeToHide = 0f; // Tiempo para ocultar el Objeto Movimiento
+        private float movementTimeToHideTimer = Mathf.Infinity;
         private bool movementDone = false; // Indicaremos cuando completo el Tutorial de Movimiento
 
-        [Header("Movement")]
-        [SerializeField] private GameObject dashObject = null; // Almacenaremos el Objeto que contiene el Cartel de Movimiento
-        private bool dashDone = false; // Indicaremos cuando completo el Tutorial de Movimiento
+        [Header("Dash")]
+        [SerializeField] private GameObject dashObject = null; // Almacenaremos el Objeto que contiene el Cartel de Dash
+        [SerializeField] private float dashTimeToHide = 0f; // Tiempo para ocultar el Objeto Dash
+        private float dashTimeToHideTimer = Mathf.Infinity;
+        private bool dashDone = false; // Indicaremos cuando completo el Tutorial de Dash
 
-        [Header("Movement")]
-        [SerializeField] private GameObject healObject = null; // Almacenaremos el Objeto que contiene el Cartel de Movimiento
-        private bool healDone = false; // Indicaremos cuando completo el Tutorial de Movimiento
+        [Header("Heal")]
+        [SerializeField] private GameObject healObject = null; // Almacenaremos el Objeto que contiene el Cartel de Heal
+        [SerializeField] private float healTimeToHide = 0f; // Tiempo para ocultar el Objeto Heal
+        private float healTimeToHideTimer = Mathf.Infinity;
+        private bool healDone = false; // Indicaremos cuando completo el Tutorial de Heal
 
-        [Header("Movement")]
-        [SerializeField] private GameObject attackObject = null; // Almacenaremos el Objeto que contiene el Cartel de Movimiento
-        private bool attackDone = false; // Indicaremos cuando completo el Tutorial de Movimiento
-
-        private void Start()
-        {
-            movementObject.SetActive(true);
-        }
+        [Header("Attack")]
+        [SerializeField] private GameObject attackObject = null; // Almacenaremos el Objeto que contiene el Cartel de Attack
+        [SerializeField] private float attackTimeToHide = 0f; // Tiempo para ocultar el Objeto Attack
+        private float attackTimeToHideTimer = Mathf.Infinity;
+        private bool attackDone = false; // Indicaremos cuando completo el Tutorial de Attack
 
         private void Update()
         {
-            if (!movementDone)
+            if (!canStart && Time.time > timeToStart) // Si todavía no arranco y pasó el tiempo
             {
-                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D))
+                canStart = true; // Que puede empezar
+                movementObject.SetActive(true); // Mostramos el 1er paso del Tutorial (Movimiento)
+            }
+
+            if (!movementDone && canStart) // Si no termino el Tutorial de Movimiento y puede empezar
+            {
+                if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D)) // Si toca alguna tecla de Movimiento
                 {
-                    movementObject.SetActive(false);
-                    movementDone = true;
-                    dashObject.SetActive(true);
+                    movementTimeToHide += Time.time;
+                }
+
+                if (Time.time > movementTimeToHide)
+                {
+                    movementObject.SetActive(false); // Ocultamos el Objeto Movimiento
+                    movementDone = true; // Que termino el Tutorial de Movimiento
+                    dashObject.SetActive(true); // Mostramos el 2do paso del Tutorial (Dash)
                 }
             }
-            else if (!dashDone && movementDone)
+            else if (!dashDone && movementDone) // Si no termino el Tutorial de Dash y termino el Tutorial de Movimiento
             {
-                if (Input.GetButtonDown("Fire2"))
+                if (Input.GetButtonDown("Fire2")) // Si hace Click con el Boton Derecho del Mouse
                 {
-                    dashObject.SetActive(false);
-                    dashDone = true;
-                    healObject.SetActive(true);
+                    dashObject.SetActive(false); // Ocultamos el Objeto Dash
+                    dashDone = true; // Que termino el Tutorial de Dash
+                    healObject.SetActive(true); // Mostramos el 2do paso del Tutorial (Heal)
                 }
             }
-            else if (!healDone && dashDone)
+            else if (!healDone && dashDone) // Si no termino el Tutorial de Heal y termino el Tutorial de Dash
             {
-                if (Input.GetKeyDown(KeyCode.C))
+                //TODO: Hacer daño al Personaje para el Tutorial
+                if (Input.GetKeyDown(KeyCode.C)) // Si presiona la tecla C
                 {
-                    healObject.SetActive(false);
-                    healDone = true;
-                    attackObject.SetActive(true);
+                    healObject.SetActive(false); // Ocultamos el Objeto Heal
+                    healDone = true; // Que termino el Tutorial de Heal
+                    attackObject.SetActive(true); // Mostramos el 2do paso del Tutorial (Attack)
                 }
             }
-            else if (!attackDone && healDone)
+            else if (!attackDone && healDone) // Si no termino el Tutorial de Attack y termino el Tutorial de Heal
             {
-                if (Input.GetButtonDown("Fire1"))
+                if (Input.GetButtonDown("Fire1")) // Si hace Click con el Boton Izquierdo del Mouse
                 {
-                    attackObject.SetActive(false);
-                    attackDone = true;
-                    Destroy(this);
+                    attackObject.SetActive(false); // Ocultamos el Objeto Attack
+                    attackDone = true; // Que termino el Tutorial de Attack
+                    Destroy(this); // Destruimos el Componente para que no siga consumiento recursos
                 }
             }
         }
