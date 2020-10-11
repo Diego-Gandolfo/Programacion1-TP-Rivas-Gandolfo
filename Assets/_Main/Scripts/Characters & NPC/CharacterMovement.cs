3ece82b2f34f6d559be92e6c0c6cbbbbd30aeeeb
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2020-10-11 17:57:57 -0300

    Intento de arreglar los Materiales, Mejora del Movement por Velocity

--- Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
+++ Assets/_Main/Scripts/Characters & NPC/CharacterMovement.cs
@@ -16,20 +16,36 @@ namespace OnceUponAMemory.Main
 
         [SerializeField] private ParticleSystem dustPS = null;
 
-        
+        private Rigidbody2D myRigidbody2D = null;
+
+        private void Awake()
+        {
+            myRigidbody2D = GetComponent<Rigidbody2D>();
+        }
+        /*
         private void Update()
         {
-            // Movimiento
-            
+            // Movimiento por Transltate
             Vector2 speedMov = new Vector2(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime); // Almacenamos el Eje Vertical y el Eje Horizontal multiplicando por la Velocidad de Movimiento y lo normalizamos con Time.deltaTime
             transform.Translate(speedMov); // Nos movemos en el Eje Vertical y Horizontal
             
             animatorMovement.SetFloat("Speed", speedMov.sqrMagnitude); // Definimos si se está Moviendo o no
-            
-            CreateDust();
-            
-            //TODO: Checkear el Dust
-            // Hacerlo con un Instantiate solo cuando se esté moviendo
+
+            if (speedMov.magnitude != 0) CreateDust();
+        }
+        */
+        private void FixedUpdate()
+        {
+
+            // Movement por Rigidbody.velocity
+            float xMovement = Input.GetAxis("Horizontal") * (movementSpeed);
+            float yMovement = Input.GetAxis("Vertical") * (movementSpeed);
+
+            Vector2 movement = new Vector2(xMovement, yMovement);
+
+            myRigidbody2D.velocity = movement;
+            animatorMovement.SetFloat("Speed", movement.sqrMagnitude);
+            if (xMovement != 0 || yMovement != 0) CreateDust();
         }
 
         private void CreateDust()
