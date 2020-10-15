36feebb051876a74567388c7c1d64425b788a673
Franco Rivas <francorivaa19@gmail.com>
2020-10-15 13:49:36 -0300

    se empezó la lógica de la puerta, aún no funciona

---
+++ Assets/_Main/Scripts/Various/DoorScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    //Quise empezar la lógica de la puerta, por alguna razón no me reconoce el playewr cuando entra en colisión,pero lo estoy viendo estos dias
+
+
+    /*public bool hasKey;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("this is a player");
+        }
+    }
+    */
+}
