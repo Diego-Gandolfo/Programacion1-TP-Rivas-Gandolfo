eb6298c96b5195089f2f8f5a7390ee4a443cbe7d
Franco Rivas <francorivaa19@gmail.com>
2020-10-15 18:12:29 -0300

    lógica de llave y puerta!

--- Assets/_Main/Scripts/Various/DoorScript.cs
+++ Assets/_Main/Scripts/Various/DoorScript.cs
@@ -7,7 +7,7 @@ public class DoorScript : MonoBehaviour
     //Quise empezar la lógica de la puerta, por alguna razón no me reconoce el playewr cuando entra en colisión,pero lo estoy viendo estos dias
 
 
-    /*public bool hasKey;
+    public bool hasKey;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +23,11 @@ public class DoorScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
-        {
-            Debug.Log("this is a player");
-        }
+        if(other.CompareTag("Player") && !hasKey)
+            Debug.Log("you need a key");
+        
+        else if(other.CompareTag("Player") && hasKey)
+            Debug.Log("you can pass now");
+             
     }
-    */
 }
---
+++ Assets/_Main/Scripts/Various/Key.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public DoorScript door;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            door.hasKey = true;
+
+            Debug.Log("now you have a key");
+            gameObject.SetActive(false);
+        }
+    }
+}
