d44328769ea8e1c48cd9e66070c61611d413760e
Franco Rivas <francorivaa19@gmail.com>
2020-10-18 10:02:29 -0300

    ()

--- Assets/_Main/Scripts/Various/Grenade.cs
+++ Assets/_Main/Scripts/Various/Grenade.cs
@@ -7,6 +7,7 @@ namespace OnceUponAMemory.Main
     public class Grenade : MonoBehaviour
     {
         public bool hasGrenade;
+
         private void OnCollisionEnter2D(Collision2D collision)
         {
             if(collision.gameObject.CompareTag("Player"))
