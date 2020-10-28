19164281a163434bb9a8ed9f572482d21c1bdb47
Franco Rivas <francorivaa19@gmail.com>
2020-10-28 14:02:39 -0300

    parallax en el menú!

---
+++ Assets/_Main/Scripts/Various/CameraMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    private float speed = 1f;
+
+    void Update()
+    {
+        transform.position += new Vector3(speed, 0f, 0f) * Time.deltaTime;
+    }
+}
