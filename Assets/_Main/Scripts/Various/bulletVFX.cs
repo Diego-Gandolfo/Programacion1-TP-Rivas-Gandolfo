a498a4b41d62733d8c7d187a2d4319ffb8e6a8e1
Franco Rivas <francorivaa19@gmail.com>
2020-10-29 18:29:20 -0300

    bullet VFX!

---
+++ Assets/_Main/Scripts/Various/bulletVFX.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletVFX : MonoBehaviour
+{
+    private float lifeTime = 0.2f;
+    private float currentLifetime = 0.0f;
+
+    void Update()
+    {
+        currentLifetime += Time.deltaTime;
+
+        if (currentLifetime >= lifeTime)
+            Destroy(gameObject);
+    }
+}
