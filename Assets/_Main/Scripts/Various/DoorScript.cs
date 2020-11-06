f01dd736ccf2e158a819aa18804cc2f7e7d15c6d
Franco Rivas <francorivaa19@gmail.com>
2020-11-06 15:29:21 -0300

    animación de la puerta!

--- Assets/_Main/Scripts/Various/DoorScript.cs
+++ Assets/_Main/Scripts/Various/DoorScript.cs
@@ -10,24 +10,51 @@ namespace OnceUponAMemory.Main
 
         private Animator animator;
 
+        private Collider2D doorCol;
+
         private void Awake()
         {
             animator = GetComponent<Animator>();
+            doorCol = GetComponent<Collider2D>();
         }
 
-        private void OnTriggerEnter2D(Collider2D other)
+        /*private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player") && !hasKey)
-                Debug.Log("you need a key");
-
-            else if (other.CompareTag("Player") && hasKey)
             {
-                Debug.Log("you can pass now");
+                animator.SetBool("CanOpen", false);
 
-                animator.SetBool("CanOpen", true);
+                Debug.Log("you need a key");
             }
                 
 
+            else if (other.CompareTag("Player") && hasKey)
+            {
+                animator.SetBool("CanOpen", true);
+
+                Debug.Log("you can pass now");
+            }
         }
+        */
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player") && !hasKey)
+            {
+                animator.SetBool("CanOpen", false);
+
+                Debug.Log("you need a key");
+            }
+            else if (collision.gameObject.CompareTag("Player") && hasKey)
+            {
+                animator.SetBool("CanOpen", true);
+
+                Debug.Log("you can pass now");
+
+                doorCol.enabled = false;
+            }
+        }
+
     }
+
+
 }
