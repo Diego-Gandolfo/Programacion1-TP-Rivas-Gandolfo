9b3ee7a593a70be655616bf58f9f245512a5099e
Franco Rivas <francorivaa19@gmail.com>
2020-11-11 16:17:08 -0300

    lógica de la llave dividida en 2!

--- Assets/_Main/Scripts/Various/Key.cs
+++ Assets/_Main/Scripts/Various/Key.cs
@@ -6,11 +6,7 @@ namespace OnceUponAMemory.Main
 {
     public class Key : MonoBehaviour
     {
-        [SerializeField] private DoorScript door = null;
-
         private Vector3 rotation = Vector3.zero;
-
-        [SerializeField] private GameObject keyIcon = null; 
 
         private void Start()
         {
@@ -20,23 +16,6 @@ namespace OnceUponAMemory.Main
         {
             transform.Rotate(rotation);
         }
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.CompareTag("Player"))
-            {
-                door.hasKey = true;
-                keyIcon.gameObject.SetActive(true);
-
-                Debug.Log("now you have a key");
-
-                gameObject.SetActive(false);
-
-                SoundManager.PlaySound("KeyPickUp");
-            }
-            else
-            {
-                keyIcon.gameObject.SetActive(false);
-            }
-        }
+        
     }
 }
---
+++ Assets/_Main/Scripts/Various/KeyCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OnceUponAMemory.Main
+{
+    public class KeyCollision : MonoBehaviour
+    {
+        [SerializeField] private GameObject keyIcon = null;
+        [SerializeField] private DoorScript door = null;
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                door.hasKey = true;
+                keyIcon.gameObject.SetActive(true);
+
+                Debug.Log("now you have a key");
+
+                gameObject.SetActive(false);
+
+                SoundManager.PlaySound("KeyPickUp");
+            }
+            else
+            {
+                keyIcon.gameObject.SetActive(false);
+            }
+        }
+    }
+}
