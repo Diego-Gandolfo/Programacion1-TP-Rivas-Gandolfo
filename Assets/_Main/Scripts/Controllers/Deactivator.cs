16d3af181d5ca38a7b17fb31b0c053b6c41678b0
Franco Rivas <francorivaa19@gmail.com>
2020-11-28 22:44:10 -0300

    Chain importada!

--- Assets/_Main/Scripts/Controllers/Deactivator.cs
+++ Assets/_Main/Scripts/Controllers/Deactivator.cs
@@ -14,6 +14,19 @@ namespace OnceUponAMemory.Main
 
         public bool hasBlockKey;
 
+        [SerializeField]
+        private Animator animator;
+
+        private bool canCount = false;
+
+        private float timeToDestroy = 1f;
+        private float currentTimeToDestroy;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
         private void Start()
         {
             hasBlockKey = false;
@@ -30,13 +43,29 @@ namespace OnceUponAMemory.Main
                 hasBlockKey = true;
                 gameObject.SetActive(false);
 
-                cutsceneTrigger.gameObject.SetActive(true);
+                animator.SetTrigger("Broken");
+
+                canCount = true;
             }
 
             else
             {
                 hasBlockKey = false;
                 cutsceneTrigger.gameObject.SetActive(false);
+            }
+        }
+
+        private void Update()
+        {
+            if (canCount)
+            {
+                currentTimeToDestroy += Time.deltaTime;
+
+                if (currentTimeToDestroy >= timeToDestroy)
+                {
+                    Debug.Log("averga");
+                    cutsceneTrigger.gameObject.SetActive(true);
+                }
             }
         }
     }
