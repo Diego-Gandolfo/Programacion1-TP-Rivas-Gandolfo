77f75fecb80b4d45dd0f6d87fea2bbf0c6d9128d
Franco Rivas <francorivaa19@gmail.com>
2020-12-02 12:38:55 -0300

    empezamos a encarar el tutorial!

---
+++ Assets/_Main/Scripts/Various/TutorialTrgger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrgger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            text.gameObject.SetActive(true);
+        }
+        else
+        {
+            text.gameObject.SetActive(false);
+        }
+    }
+}
