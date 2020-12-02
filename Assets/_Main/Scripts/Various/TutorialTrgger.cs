1df0ebbb6db45802f745639f65a1c02dc65f1b00
Franco Rivas <francorivaa19@gmail.com>
2020-12-02 13:57:14 -0300

    avanzamos con el tutorial!

--- Assets/_Main/Scripts/Various/TutorialTrgger.cs
+++ Assets/_Main/Scripts/Various/TutorialTrgger.cs
@@ -7,13 +7,40 @@ public class TutorialTrgger : MonoBehaviour
     [SerializeField]
     private GameObject text;
 
+    private float timeToGo = 6.5f;
+
+    [SerializeField]
+    private float currentTimeToGo = 0.0f;
+
+    private bool canCount = false;
+
+    private void Update()
+    {
+        if (canCount)
+        {
+            currentTimeToGo += Time.deltaTime;
+
+            if (currentTimeToGo >= timeToGo)
+            {
+                text.SetActive(false);
+
+                currentTimeToGo = 0.0f;
+                canCount = false;
+
+                Destroy(gameObject);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("LOLARDO PAPA");
             text.SetActive(true);
+
+            canCount = true;
         }
+
         else
         {
             text.SetActive(false);
