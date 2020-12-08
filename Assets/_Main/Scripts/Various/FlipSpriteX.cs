397fb74d0c53c99601213fea3b4d2791a20d8f94
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2020-12-08 02:57:47 -0300

    Solucionado el problema del EnemySummon y el FlipSpriteX

--- Assets/_Main/Scripts/Various/FlipSpriteX.cs
+++ Assets/_Main/Scripts/Various/FlipSpriteX.cs
@@ -21,10 +21,13 @@ namespace OnceUponAMemory.Main
 
         private void Update()
         {
-            if (transform.position.x > target.transform.position.x) // Chequeamos si nuestra posicion en Eje X es MAYOR que la del Player
-                spriteRenderer.flipX = true; // Volteamos
-            else // Si no es MENOR
-                spriteRenderer.flipX = false; // No volteamos
+            if (spriteRenderer != null && target != null)
+            {
+                if (transform.position.x > target.transform.position.x) // Chequeamos si nuestra posicion en Eje X es MAYOR que la del Player
+                    spriteRenderer.flipX = true; // Volteamos
+                else // Si no es MENOR
+                    spriteRenderer.flipX = false; // No volteamos
+            }
         }
     }
 }
